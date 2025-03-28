105f7629dfb27d7764fbd74248ed47fc4784e974
nikolaizombie1 <fabio.matos999@gmail.com>
2025-03-28 15:34:12 -0400

    Initial Commit

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... } @ inputs: {
+
+nixosConfigurations.uwu = nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs;};
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
